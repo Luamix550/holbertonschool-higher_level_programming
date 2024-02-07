933c71ad865a7a3f4d49d81580962d1b1fc7c81f
Luamix <luizalfonso611@gmail.com>
2024-02-07 18:01:09 -0500

    changes in my all files

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i == 99:
-        print(i)
-    else:
-        print("{:02d}, ".format(i), end="")
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
