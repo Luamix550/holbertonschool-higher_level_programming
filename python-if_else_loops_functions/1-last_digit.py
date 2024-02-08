0901de494403d36596403b64f97ce2bd90721f18
Luamix <luizalfonso611@gmail.com>
2024-02-08 12:05:02 -0500

    resolve conflict in taks 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit}", end=" ")
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+if number < 0:
+    last_number = number % -10
 else:
-    print("and is less than 6 and not 0")
+    last_number = number % 10
+print('Last digit of {} is '.format(number), end="")
+if last_number > 5:
+    print('{} and is greater than 5'.format(last_number))
+elif last_number == 0:
+    print('{} and is 0'.format (last_number))
+elif last_number < 6 and last_number != 0:
+    print('{} and is less than 6 and not 0'.format(last_number))
+
