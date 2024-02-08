2c1cb1a46c4ab5c59cb853fe97d0db2f553afd3d
Luamix <luizalfonso611@gmail.com>
2024-02-08 12:06:01 -0500

    resolve conflict in taks 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,6 @@ print('Last digit of {} is '.format(number), end="")
 if last_number > 5:
     print('{} and is greater than 5'.format(last_number))
 elif last_number == 0:
-    print('{} and is 0'.format (last_number))
+    print('{} and is 0'.format(last_number))
 elif last_number < 6 and last_number != 0:
     print('{} and is less than 6 and not 0'.format(last_number))
-
