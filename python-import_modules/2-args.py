39294d21c72c3df789dc0565a830864f0bb2afda
Luamix <luizalfonso611@gmail.com>
2024-02-09 17:42:33 -0500

    taks 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    number_arg = len(sys.argv) - 1
-    args_list = sys.argv[1:]
-
-    if number_arg == 0:
-        print(f"{number_arg} arguments.")
-    elif number_arg == 1:
-        print(f"{number_arg} argument:")
+    length = len(sys.argv)
+    if length == 1:
+        print("{} arguments.".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
     else:
-        print(f"{number_arg} arguments:")
-    if number_arg:
-        for i, arg in enumerate(args_list, 1):
-            print(f"{i}: {arg}")
+        print("{} arguments:".format(length - 1))
+    for i in range(1, length):
+        print("{}: {}".format(i, sys.argv[i]))
