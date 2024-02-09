81b1c26532e2e25120e24369b20049cb2ee543c4
Luamix <luizalfonso611@gmail.com>
2024-02-08 19:09:33 -0500

    task 1

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
+from add_0 import add
+
+def main(a, b):
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a,b)))
+
+if __name__ == "__main__":
+    main()
