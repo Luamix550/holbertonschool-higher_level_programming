3305b2953660deb84f460538a8a856f7488e9f24
Luamix <luizalfonso611@gmail.com>
2024-02-10 21:47:42 -0500

    fix pycodestyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
-    j = 0
-    for i in range(1, args):
-        j += (int(sys.argv[i]))
-    print("{}".format(j))
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__ == '__main__':
-    for name in dir(hidden_4):
-        if ("__" != name[:2]):
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
