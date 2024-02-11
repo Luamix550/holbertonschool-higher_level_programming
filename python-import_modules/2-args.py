70ef016d25c484bdcef6e229562e7a07f2663fa9
Luamix <luizalfonso611@gmail.com>
2024-02-10 21:52:01 -0500

    pycodestyle fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == '__main__':
-    args = argv[1:]
-    num_args = len(args)
-    if (num_args == 0):
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif (num_args == 1):
+    elif count == 1:
         print("1 argument:")
     else:
-        print(f"{num_args} arguments:")
-    for i in range(num_args):
-        print(f"{i + 1}: {args[i]}")
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
