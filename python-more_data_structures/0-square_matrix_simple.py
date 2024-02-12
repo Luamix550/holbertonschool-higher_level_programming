ce3805d51b847697c0b808bae13d1de076c61170
Luamix <luizalfonso611@gmail.com>
2024-02-12 11:12:22 -0500

    task 1

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,3 @@
 def square_matrix_simple(matrix=[]):
     squared_matrix_simple = [[element**2 for element in row] for row in matrix]
     return squared_matrix_simple
-    result = square_matrix_simple()
-    print(result)
