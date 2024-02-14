9f6fb22d2d0d0252cceb15fb46eaeb46b11811ce
Luamix <luizalfonso611@gmail.com>
2024-02-14 18:44:28 -0500

    return div

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,14 +6,4 @@ def safe_print_division(a, b):
         div = None
     finally:
         print("Inside result: {}".format(div))
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+        return div
