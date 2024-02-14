012098aa6eae90d0ea4385fd8d582e334819c2c4
Luamix <luizalfonso611@gmail.com>
2024-02-14 18:42:21 -0500

    fix pycodestyle

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0;
-    try: 
+    try:
         div = a / b
     except (ZeroDivisionError):
         div = None
     finally:
-            print("Inside result: {}".format(div))
+        print("Inside result: {}".format(div))
+
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
