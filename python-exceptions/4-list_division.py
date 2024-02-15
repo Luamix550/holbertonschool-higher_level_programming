591efc966aa15cab61494002ab2dde471a4c2d52
Luamix <luizalfonso611@gmail.com>
2024-02-14 20:48:50 -0500

    fix pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
-    for i in range(list_length):
+    div = []
+    temp_div = 0
+    for i in range(0, list_length):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError("out of range")
-            try:
-                quotient = my_list_1[i] / my_list_2[i]
-            except ZeroDivisionError:
-                print("division by 0")
-                quotient = 0
-            except TypeError:
-                print("wrong type")
-                quotient = 0
-        except IndexError as e:
-            print(e)
-            break
+            temp_div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            temp_div = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            temp_div = 0
+            print("division by 0")
+        except IndexError:
+            temp_div = 0
+            print("out of range")
         finally:
-            result.append(quotient)
-    return result
+            pass
+        div.append(temp_div)
+    return div
