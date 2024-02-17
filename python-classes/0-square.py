85402eed329bf20ab8c7042c992de8651f363435
Luamix <luizalfonso611@gmail.com>
2024-02-17 12:48:02 -0500

    add documentation inside the code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module contains an empty class Square.
+"""
 
 
 class Square:
+    """
+    This is an empty class Square.
+    """
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+This module contains a class Square that defines a square.
+"""
+
+
 class Square:
+    """
+    This class defines a square with a private instance attribute size.
+    """
 
     def __init__(self, size):
+        """
+        Initializes a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The size of the square.
+        """
         self.__size = size
