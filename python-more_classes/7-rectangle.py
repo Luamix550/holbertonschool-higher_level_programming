0224f91f608b81584417c45e85a1d5ec669adf81
Luamix <luizalfonso611@gmail.com>
2024-02-21 13:12:23 -0500

    task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
-This is Rectangle a "Rectangle" module.
+This is the "Rectangle"  module.
 
-This module provides a simple Rectangle class with attribute width and height.
-Default values of both attributes are 0.
+This module provides a Rectangle class.
 """
 
 
 class Rectangle:
-    """A Rectangle class with attributes  width and height"""
+    """A Rectangle class with attributes width and height,
+    methods area, perimeter, print, str, repr, and del, and
+    class attribute number_of_instances that keeps track of # of instances.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -19,7 +26,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +38,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -42,18 +49,24 @@ class Rectangle:
 
     def __str__(self):
         total = ""
-        if self.__height == 0 or self.width == 0:
-            return total
         for i in range(self.__height):
-            total += ("#" * self.__width)
+            for j in range(self.__width):
+                try:
+                    total += str(self.print_symbol)
+                except Exception:
+                    total += type(self).print_symbol
             if i is not self.__height - 1:
                 total += "\n"
         return total
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
         return (2 * self.__width) + (2 * self.__height)
