6788800b3dd2be8a15f6d4e0e9864dc1f69365c8
Luamix <luizalfonso611@gmail.com>
2024-02-23 22:00:48 -0500

    input output

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Module that contains a function that appends to a text file
+""" Module that contains a function that returns the JSON
+representation of an object
 """
+import json
 
 
-def append_write(filename="", text=""):
-    """ Function that appends to a text file
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
 
     Args:
-        filename: filename
-        text: text to write
+        my_obj: object
 
-    Raises
-        Exception: when the file can be opened
+    Raises:
+        Exception: when the object can't be encoded
 
     """
-
-    with open(filename, 'a', encoding="utf-8") as f:
-        return f.write(text)
+    return json.dumps(my_obj)
