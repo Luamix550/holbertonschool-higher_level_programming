795c94da5fbed1e9550c6a517f8443e1491cc3bd
Luamix <luizalfonso611@gmail.com>
2024-02-23 22:06:09 -0500

    input output

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+"""Class that defines a student"""
 
 
 class Student:
-    """ Class to create student instances """
+    """Represents a student"""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Constructor - Initialize a new student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+    def to_json(self, attrs=None):
+        """Retrieves a dict representation of a instance"""
+        dict = self.__dict__
+        if attrs is None:
+            return dict
+
+        """Retrieves the attributes that are in a list"""
+        new_dict = {}
+        for attr in dict.keys():
+            if attr in attrs:
+                new_dict[attr] = dict[attr]
+        return new_dict
