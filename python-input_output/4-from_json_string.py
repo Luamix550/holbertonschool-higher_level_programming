ca61e72b8489db8fbf321e5438fa71e46d961b80
Luamix <luizalfonso611@gmail.com>
2024-02-23 21:48:12 -0500

    input output

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines a JSON-to-object function."""
 import json
 
-
-def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+    """
+    return json.loads(my_str)
