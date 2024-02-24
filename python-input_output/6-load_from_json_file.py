d210cc62151a36111ac27262f6ac1deeb8c53628
Luamix <luizalfonso611@gmail.com>
2024-02-23 21:56:30 -0500

    input output

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""Defines a JSON file-writing function."""
+""" Module that contains a function that returns an object by
+a JSON representation
+"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+def from_json_string(my_str):
+    """ Function that returns an object by a JSON representation
+
+    Args:
+        my_str: JSON representation
+
+    Raises:
+        Exception: when the string can't be decoded
+
+    """
+    return json.loads(my_str)
