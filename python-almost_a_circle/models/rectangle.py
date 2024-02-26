c73bea8609dd46ab1dc2e44a366086340ed5d489
Luamix <luizalfonso611@gmail.com>
2024-02-26 17:59:04 -0500

    add more documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,45 +1,94 @@
 #!/usr/bin/python3
+"""This is the Rectangle module.
+
+Contains the Rectangle class that inherits from Base.
+"""
 from models.base import Base
-    
+
+
 class Rectangle(Base):
-    """Rectangle class, inherits from Base."""
+    """This class inherits from Base and defines a Rectangle object.
+
+    Attributes:
+        __width (int): the width of the rectangle.
+        __height (int): the height of the rectangle.
+        __x (int): the horizontal (x) padding of the rectangle.
+        __y (int): the vertical (y) padding of the rectangle.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize Rectangle instance."""
-        super.__init__(id)
+        """Initializes the default attributes of the Base object.
+
+        Args:
+            width (int): the wanted width of the rectangle.
+            height (int): the wanted height of the rectangle.
+            x (int): the wanted horizontal (x) padding of the rectangle.
+            y (int): the wanted vertical (y) padding of the rectangle.
+            id (int): the wanted identifier of the Base object.
+        """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-    
+
+    def __str__(self):
+        """Overrides the default behaviour of the __str__ method."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+
+    # width attribute getter and setter.
     @property
     def width(self):
+        """Get and Set the width attribute of the Rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
-    
+
+    # height attribute getter and setter.
     @property
     def height(self):
+        """Get and Set the height attribute of the Rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
-    
+
+    # x attribute getter and setter.
     @property
     def x(self):
+        """Get and Set the x attribute of the Rectangle."""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
-    
+
+    # y attribute getter and setter.
     @property
     def y(self):
+        """Get and Set the y attribute of the Rectangle."""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
