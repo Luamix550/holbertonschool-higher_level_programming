29c5efef66f9f9797f63c69374430be1297f6ff7
Luamix <luizalfonso611@gmail.com>
2024-02-26 17:48:39 -0500

    delete documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,40 +14,32 @@ class Rectangle(Base):
     
     @property
     def width(self):
-        """Getter method for width attribute."""
         return self.__width
     
     @width.setter
     def width(self, value):
-        """Setter method for width attribute."""
         self.__width = value
     
     @property
     def height(self):
-        """Getter method for height attribute."""
         return self.__height
     
     @height.setter
     def height(self, value):
-        """Setter method for height attribute."""
         self.__height = value
     
     @property
     def x(self):
-        """Getter method for x attribute."""
         return self.__x
     
     @x.setter
     def x(self, value):
-        """Setter method for x attribute."""
         self.__x = value
     
     @property
     def y(self):
-        """Getter method for y attribute."""
         return self.__y
     
     @y.setter
     def y(self, value):
-        """Setter method for y attribute."""
         self.__y = value
