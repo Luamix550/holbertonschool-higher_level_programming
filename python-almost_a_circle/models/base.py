cf8af5fbc153873fbd8111c5aa74aabb563384ff
Luamix <luizalfonso611@gmail.com>
2024-03-04 16:05:47 -0500

    delete method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,67 +1,29 @@
 #!/usr/bin/python3
-import json
-"""Import the json module to work with JSON
+"""
+Defines the Base class with a private class
 """
 
 
 class Base:
-    """Base model for other classes."""
+    """
+    Represents a generic base class.
 
+    Attributes:
+        __nb_objects (int): Private class attribute
+    Methods:
+        __init__: Initializes an instance of Base.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance."""
+        """
+        Initializes an instance of Base.
+
+        Args:
+            id (int, optional): ID for the instance
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Convert list of dictionaries to JSON string."""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Save list of instances to a JSON file."""
-        filename = cls.__name__ + ".json"
-        with open(filename, "w") as jsonfile:
-            if list_objs is None:
-                jsonfile.write("[]")
-            else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
-                jsonfile.write(cls.to_json_string(list_dicts))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Parse JSON string to list of dictionaries."""
-        if json_string is None or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Return instance from dictionary of attributes."""
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                new = cls(1, 1)
-            else:
-                new = cls(1)
-            new.update(**dictionary)
-            return new
-
-    @classmethod
-    def load_from_file(cls):
-        """Load instances from a JSON file."""
-        filename = cls.__name__ + ".json"
-        try:
-            with open(filename, "r") as file:
-                json_string = file.read()
-                data = cls.from_json_string(json_string)
-                instances = [cls.create(**obj) for obj in data]
-                return instances
-        except FileNotFoundError:
-            return []
