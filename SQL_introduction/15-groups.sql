dfe185818e1f440574982abf2606b9559e473403
Luamix550 <luizalfonso611@gmail.com>
2024-03-31 00:12:52 +0000

    task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -2,5 +2,5 @@
 -- The result should display:
 -- the score
 -- the number of records for this score with the label number
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
 
