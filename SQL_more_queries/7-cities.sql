ddaddc427251078f4b4ee56657ae19aee1e9b1d2
Luamix <luizalfonso611@gmail.com>
2024-03-31 16:15:32 -0500

    task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+--  creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES id(states),
+	name VARCHAR(256) NOT NULL
+);
