f20c8e44f03bbf8c0df968dcaaa3588071bcf7fa
Luamix <luizalfonso611@gmail.com>
2024-03-31 15:49:21 -0500

    task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- creates the database htbn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL;
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY;
 	name VARCHAR(256)
-	PRIMARY KEY (id)
 );
 
