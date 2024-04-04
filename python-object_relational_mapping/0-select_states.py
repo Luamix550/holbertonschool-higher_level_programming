d82e83e774358d3197af42bf4389c5e0eae3e764
Luamix <luizalfonso611@gmail.com>
2024-04-04 15:40:44 -0500

    permission to 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     mysql_username, mysql_password, database_name = argv[1],  argv[2], argv[3];
     db = MySQLdb.connect(
         host="localhost",
-        port="3306",
+        port=3306,
         user=mysql_username,
         passwd=mysql_password,
         db=database_name
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
-    rows = cur.fetcall()
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
