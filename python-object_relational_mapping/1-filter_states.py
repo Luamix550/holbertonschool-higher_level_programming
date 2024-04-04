d47fd277b33f1ab432faaf7a67f3ea89279a4fdd
Luamix <luizalfonso611@gmail.com>
2024-04-04 16:20:08 -0500

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python3
 from sys import argv
 import MySQLdb
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+
+"""
+
 
 if __name__ == '__main__':
     mysql_username, mysql_password, database_name = argv[1], argv[2], argv[3]
